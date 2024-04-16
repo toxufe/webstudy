addead8caf4990f3daaf276da05d8bc5d7ae4287
Fly <723887809@qq.com>
2024-04-16 15:33:50 +0000

    2024年4月16日 23:33

--- 04.TypeScript/P20.混入/index.ts
+++ 04.TypeScript/P20.混入/index.ts
@@ -2,4 +2,30 @@
 // 1. 对象的混入 合并 A对象 B对象 合并到一起
 // 2. 类的混入 A类 B类 合并到一起
 
-// 扩展运算符 浅拷贝
+
+// 对象
+// a. 扩展运算符 浅拷贝 返回新的数据类型
+interface A {
+    name:string;
+}
+
+interface B {
+    age:number;
+}
+
+let a:A = {
+    name:'zhangsan'
+}
+
+let b:B = {
+    age:18
+}
+
+let c = {...a,...b};
+console.log('c: ', c);
+
+// b. Object.assign 浅拷贝 返回的是交叉类型
+let d = Object.assign(a,b);
+console.log('d: ', d);
+
+// c. 深拷贝

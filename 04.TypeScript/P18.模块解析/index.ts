bc988cd9af429fcde58f2f0752ec1f7a8934ac2d
Fly <723887809@qq.com>
2024-04-16 16:04:26 +0800

    模块导出

--- 04.TypeScript/P18.模块解析/index.ts
+++ 04.TypeScript/P18.模块解析/index.ts
@@ -1,4 +1,21 @@
 // 1. 默认导出的引入方式 使用import 名字随便起
-import a from './test';
-console.log('a: ', a);
+// import obj,{a,arr,add,a1 as xxx} from './test';
+// import obj,{a,arr,add} from './test';
 
+
+
+
+// console.log('obj: ', obj);
+
+// // 2. 分别导出的引入方式 使用import {名字} from '路径'
+// console.log('a: ', a);
+// console.log('arr: ', arr);
+// console.log('add: ', add(1,2));
+
+// 3. 解构导出的引入方式 使用import {名字 as 别名} from '路径'
+// console.log('a1: ', xxx);
+
+
+// 查看导出的所有内容
+import * as api from './test';
+console.log('api: ', api);
--- 04.TypeScript/P18.模块解析/test.ts
+++ 04.TypeScript/P18.模块解析/test.ts
@@ -1,4 +1,7 @@
-// 默认导出 导出的东西可以是任意类型
+// 1. 默认导出 导出的东西可以是任意类型
+// 一个模块只能有一个默认导出
+
+
 // export default 1;
 // export default ['1',2];
 
@@ -7,3 +10,20 @@ export default {
     age: 18
     
 }
+
+// 2. 分别导出
+export let a =1;
+export let arr = [1,2,3];
+export const add = (a:number,b:number)=>{
+    return a+b;
+}
+
+
+// 3. 解构导出
+// export {
+//     a as a1,
+//     arr as arr1,
+//     add as add1
+// }
+
+

d415928ab5e4285248108165f21013a773e53324
Fly <723887809@qq.com>
2024-04-17 16:32:57 +0000

    2024年4月18日 00:32

--- 05.webpack/webpack01/src/App.vue
+++ 05.webpack/webpack01/src/App.vue
@@ -1,9 +1,11 @@
-<script setup >
-
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
 </script>
 
 <template>
-  <div class="">11111111111111111</div>
+  <div class="">{{ count }}</div>
+  <button @click="count++">+1</button>
 </template>
 
 <style scoped></style>

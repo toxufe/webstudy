d0d9e4e5264973384e1fad19ac773a742f0c21d9
Fly <723887809@qq.com>
2024-04-17 00:03:19 +0000

    2024年4月17日 08:03

--- 04.TypeScript/P20.混入/index.ts
+++ 04.TypeScript/P20.混入/index.ts
@@ -3,32 +3,39 @@
 // 2. 类的混入 A类 B类 合并到一起
 
 
-// 对象
+// 对象 非常简单
 // a. 扩展运算符 浅拷贝 返回新的数据类型
-interface A {
-    name:string;
-}
+// interface A {
+//     name:string;
+// }
 
-interface B {
-    age:number;
-}
+// interface B {
+//     age:number;
+// }
 
-let a:A = {
-    name:'zhangsan'
-}
+// let a:A = {
+//     name:'zhangsan'
+// }
 
-let b:B = {
-    age:18
-}
+// let b:B = {
+//     age:18
+// }
 
-let c = {...a,...b};
-console.log('c: ', c);
+// let c = {...a,...b};
+// console.log('c: ', c);
 
-// b. Object.assign 浅拷贝 返回的是交叉类型
-let d = Object.assign(a,b);
-console.log('d: ', d);
+// // b. Object.assign 浅拷贝 返回的是交叉类型
+// let d = Object.assign(a,b);
+// console.log('d: ', d);
 
-// c. 深拷贝  
-let c1 = structuredClone(a)
+// // c. 深拷贝  
+// let c1 = structuredClone(a)
 
 // 类的混入
+// 1. 插件类型的混入
+
+class App {
+    run(){
+        console.log('run');
+    }
+}

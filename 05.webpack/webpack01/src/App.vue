81f3d6205e2dba3ed101d7116086e6a100ee7196
Fly <723887809@qq.com>
2024-04-18 14:42:59 +0000

    2024年4月18日 22:42

--- 05.webpack/webpack01/src/App.vue
+++ 05.webpack/webpack01/src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import moment from 'moment'
 const count = ref<number>(0)
 </script>
 
 <template>
   <div class="">{{ count }}</div>
   <button @click="count++">+1</button>
+
+  <div>{{ moment().format('YYYY-MM-DD HH:mm:ss') }}</div>
+
 </template>
 
 <style lang="scss">

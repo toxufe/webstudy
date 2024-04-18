848f80bccb44a5b8b6273cceed415eeec6959682
Fly <723887809@qq.com>
2024-04-18 15:59:24 +0000

    2024年4月18日 23:59

--- 04.TypeScript/P22.es6新增数据类型Map等/index.ts
+++ 04.TypeScript/P22.es6新增数据类型Map等/index.ts
@@ -1,1 +1,7 @@
 // weakMap Map weakSet Set
+// Map 哈希机构  类型对象
+// Set 集合 类似数组，但是成员的值都是唯一的
+
+// Set 天然去重 引用类型除外
+let set:Set<number> = new Set([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9]);
+console.log('set: ', set);

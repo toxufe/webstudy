10beb8139700eed160ffaba3fc242e32e46c9db4
Fly <723887809@qq.com>
2024-04-18 15:07:56 +0000

    2024年4月18日 23:07

--- 04.TypeScript/P21.发布订阅模式/index.ts
+++ 04.TypeScript/P21.发布订阅模式/index.ts
@@ -2,4 +2,15 @@
 // 设计模式 工厂、构造器、观察者、发布订阅、单例、策略、代理、迭代器、装饰器、适配器、门面、模板方法、命令、职责链、状态、备忘录、访问者、中介者、解释器
 
 // 面试、手写、思想被广泛使用
-console.log('发布订阅模式');
+// document.addEventListener('click', (e) => {
+//     console.log('e: ', e);
+
+// })
+
+// 创建自定义事件
+// 监听器
+document.addEventListener('myEvent', (e) => {
+    console.log('e: ', e);
+})
+
+const e = new Event('myEvent');// 订阅中心

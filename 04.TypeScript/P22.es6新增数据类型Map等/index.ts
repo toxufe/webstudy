dcbfdb9c2fb192f8a6ad457e57adb0529b991e5d
Fly <723887809@qq.com>
2024-04-18 16:00:59 +0000

    2024年4月19日 00:00

--- 04.TypeScript/P22.es6新增数据类型Map等/index.ts
+++ 04.TypeScript/P22.es6新增数据类型Map等/index.ts
@@ -4,4 +4,16 @@
 
 // Set 天然去重 引用类型除外
 let set:Set<number> = new Set([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9]);
+
+// 增
+set.add(10);
+// 查
+console.log('has: ', set.has(10));
+
 console.log('set: ', set);
+
+
+
+
+// package.json "type": "module",
+// ts-node-esm index.ts

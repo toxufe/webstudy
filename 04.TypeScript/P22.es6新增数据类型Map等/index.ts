e24e0f0a02422061a0fb86ba1a523871b67933e8
Fly <723887809@qq.com>
2024-04-18 16:06:48 +0000

    2024年4月19日 00:06

--- 04.TypeScript/P22.es6新增数据类型Map等/index.ts
+++ 04.TypeScript/P22.es6新增数据类型Map等/index.ts
@@ -2,24 +2,33 @@
 // Map 哈希机构  类型对象
 // Set 集合 类似数组，但是成员的值都是唯一的
 
-// Set 天然去重 引用类型除外
+// // Set 天然去重 引用类型除外
 let set:Set<number> = new Set([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9]);
 
+// // 增
+// set.add(10);
+// // 删
+// set.delete(1);
+// // 清空
+// set.clear();
+// // 改
+// // 查
+// console.log('has: ', set.has(10));
+// console.log('set: ', set);
+
+// // 循环方法 forEach entries keys for ... of
+
+
+
+// // package.json "type": "module",
+// // ts-node-esm index.ts
+
+
+
+// Map 的 key 可以是任意类型 可以是基础类型 也可以是引用类型
+let obj = {name:"zf"};
+let map:Map<object,any> = new Map(); 
+
 // 增
-set.add(10);
-// 删
-set.delete(1);
-// 清空
-set.clear();
-// 改
-
-// 查
-console.log('has: ', set.has(10));
-
-console.log('set: ', set);
-
-
-
-
-// package.json "type": "module",
-// ts-node-esm index.ts
+map.set(obj,100);
+console.log('map: ', map);

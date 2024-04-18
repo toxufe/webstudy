e4c77e8643569d3837ca9eec2768cedcf7b71ced
Fly <723887809@qq.com>
2024-04-18 16:18:15 +0000

    2024年4月19日 00:18

--- 04.TypeScript/P22.es6新增数据类型Map等/index.ts
+++ 04.TypeScript/P22.es6新增数据类型Map等/index.ts
@@ -45,24 +45,29 @@ let set:Set<number> = new Set([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9]);
 
 // // 循环方法 forEach entries keys for ... of
 
-// weakMap weakSet 弱项 弱引用
-// 垃圾回收机制 会自动回收内存 但是不会回收弱引用的内存 适合存储临时数据
-// weakMap 和 map 的区别 1. key 只能是对象 2. key 是弱引用的
+// // weakMap weakSet 弱项 弱引用
+// // 垃圾回收机制 会自动回收内存 但是不会回收弱引用的内存 适合存储临时数据
+// // weakMap 和 map 的区别 1. key 只能是对象 2. key 是弱引用的
 
-let obj:any = {name:"zf"};// 引用一次 引用计数为1
-let xxx:any = obj;//   引用计数为2
+// let obj:any = {name:"zf"};// 引用一次 引用计数为1
+// let xxx:any = obj;//   引用计数为2
 
-let map:WeakMap<object,any> = new WeakMap();// 不会被垃圾回收机制回收 
-map.set(obj,100);
-console.log('map: ', map.get(obj));
-// console.log('map: ', map);
+// let map:WeakMap<object,any> = new WeakMap();// 不会被垃圾回收机制回收 
+// map.set(obj,100);
+// console.log('map: ', map.get(obj));
+// // console.log('map: ', map);
 
-obj = null;// 引用计数为1
-xxx = null;// 引用计数为0
-console.log('map1: ', map.get(obj));
-
-
-// 浏览器问题
+// obj = null;// 引用计数为1
+// xxx = null;// 引用计数为0
+// console.log('map1: ', map.get(obj));
 
 
+// // 浏览器问题
+// // V8 GC 的垃圾回收机制是需要时间的 会有一个延迟 至少200ms
+
+// weakSet
+let weakSet:WeakSet<object> = new WeakSet();
+let obj:any = {name:"zf"};
+weakSet.add(obj);
+console.log('weakSet: ', weakSet.has(obj));
 

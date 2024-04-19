af16f26c407215c899625904531e711fb560ba97
Fly <723887809@qq.com>
2024-04-19 05:58:34 +0000

    2024年4月19日 13:58

--- 04.TypeScript/P24.类型守卫/index.ts
+++ 04.TypeScript/P24.类型守卫/index.ts
@@ -1,6 +1,11 @@
 // 类型守卫 在代码中执行特定的检查以确定类型的方法 以及 确定变量的一个类型
 // 1. 类型收缩 | 类型收窄
-const isstring = (str:any)=> typeof str === 'string';
+
+// typeof 是有缺陷的 比如说 数组 对象 函数 null 都会返回 object
+// 判断字符串
+// const isstring = (str:any)=> typeof str === 'string';
+// 判断数组
+const isArr = (arr: any)=> arr instanceof Array;
 
 
 

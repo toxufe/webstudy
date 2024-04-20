b000dec7396d61d7f0b9573fd1599de8b612f5e8
Fly <723887809@qq.com>
2024-04-20 15:27:09 +0000

    2024年4月20日 23:27

--- 04.TypeScript/P26.进阶-泛型工具/index.ts
+++ 04.TypeScript/P26.进阶-泛型工具/index.ts
@@ -8,12 +8,12 @@
  */ 
 
 
-
-interface User {
-    name: string
-    age: number
-    address:string
-}
+// Partial
+// interface User {
+//     name: string
+//     age: number
+//     address:string
+// }
 
 // type PartialUser = Partial<User>;// 所有属性 可选的意思是 接受一个泛型，泛型里边的属性都会变成可选的属性
 // type PartialUser = {
@@ -24,9 +24,43 @@ interface User {
 
 // 自己实现一个 Partial
 // 联合类型
-type MyPartial<T> = {
-    [K in keyof T]?:T[K];
-};// 用来接收一个泛型
+// type MyPartial<T> = {
+//     [K in keyof T]?:T[K];
+// };// 用来接收一个泛型
 
-type PartialUser = MyPartial<User>;
+// type PartialUser = MyPartial<User>;
+// type PartialUser = {
+//     name?: string | undefined;
+//     age?: number | undefined;
+//     address?: string | undefined;
+// }
 
+
+// Required
+// interface User {
+//     name?: string
+//     age?: number
+//     address?:string
+// }
+// type RequiredUser = Required<User>;// 所有属性必填的意思
+// type RequiredUser = {
+//     name: string;
+//     age: number;
+//     address: string;
+// }
+
+// 自己实现一个 Required 使用减号的方式[-?]
+// type MyRequired<T> = {
+//     [K in keyof T]-?:T[K];
+// };
+// type RequiredUser = MyRequired<User>;
+// type RequiredUser = {
+//     name: string;
+//     age: number;
+//     address: string;
+// }
+
+
+//  Pick 提取部分属性
+//  Omit 忽略部分属性 并且返回新的类型
+//  Exclude 排除部分属性

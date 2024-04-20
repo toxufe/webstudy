1c2afdedf98159fc9f1d8022979482af6d6b0959
Fly <723887809@qq.com>
2024-04-20 15:57:11 +0000

    2024年4月20日 23:57

--- 04.TypeScript/P26.进阶-泛型工具/index2.ts
+++ 04.TypeScript/P26.进阶-泛型工具/index2.ts
@@ -10,37 +10,47 @@
 // 所以接收两个泛型参数
 // key 必须全部满足 value 随便 但是必须是一个类型
 
-type Key = "a"| "b" | "c";
-type Value = "1" | "2" | "3";
+// type Key = "a"| "b" | "c";
+// type Value = "1" | "2" | "3";
 
-// const obj:Record<Key,Value> = {
+// // const obj:Record<Key,Value> = {
+// //     a:"1",
+// //     b:"2",
+// //     c:"1"
+// // };
+
+
+// // 嵌套的写法
+// // const obj:Record<Key,Record<Key,Value>> = {
+// //     a:{
+// //         a:"1",
+// //         b:"2",
+// //         c:"3"
+// //     },
+// //     b:{
+// //         a:"1",
+// //         b:"2",
+// //         c:"3"
+// //     },
+// //     c:{
+// //         a:"1",
+// //         b:"1",
+// //         c:"3"
+// //     }
+// // };
+
+// // 源码的写法
+// // 对象的key只能是string或者number或者symbol
+// type ttk =  keyof any; // string或者number或者symbol
+// type CustomRecord<K extends ttk,T> = {
+//     [P in K]:T;
+// }
+// const obj:CustomRecord<Key,Value> = {
 //     a:"1",
 //     b:"2",
-//     c:"1"
+//     c:"3"
 // };
 
-
-// 嵌套的写法
-// const obj:Record<Key,Record<Key,Value>> = {
-//     a:{
-//         a:"1",
-//         b:"2",
-//         c:"3"
-//     },
-//     b:{
-//         a:"1",
-//         b:"2",
-//         c:"3"
-//     },
-//     c:{
-//         a:"1",
-//         b:"1",
-//         c:"3"
-//     }
-// };
-
-// 源码的写法
-// 对象的key只能是string或者number或者symbol
 
 
 // * 2. ReturnType

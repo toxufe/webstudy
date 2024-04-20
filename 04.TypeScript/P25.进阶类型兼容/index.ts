de07d58f92200435ef4698311b4d9829931a8a85
Fly <723887809@qq.com>
2024-04-20 03:03:44 +0000

    2024年4月20日 11:03

--- 04.TypeScript/P25.进阶类型兼容/index.ts
+++ 04.TypeScript/P25.进阶类型兼容/index.ts
@@ -3,3 +3,31 @@
 // 什么是鸭子类型？
 // 一只鸟 走路像鸭子 ，游泳也像，做什么都像，那么这只鸟就可以成为鸭子类型。
 
+// 举例
+interface A {
+    name:string
+    age:number
+    sex:string
+}
+ 
+interface B {
+    name:string
+    age:number
+   
+}
+ 
+let a:A = {
+    name:"老墨我想吃鱼了",
+    age:33,
+    sex:"女"
+}
+ 
+let b:B = {
+    name:"老墨我不想吃鱼",
+    age:33,
+   
+}
+ 
+a = b
+
+// A B 两个类型完全不同但是竟然可以赋值并无报错B类型充当A类型的子类型，当子类型里面的属性满足A类型就可以进行赋值，也就是说不能少可以多，这就是协变。

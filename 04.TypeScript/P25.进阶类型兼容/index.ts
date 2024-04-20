7e2b8478770a77eb2e0224202bb83c395437b55d
Fly <723887809@qq.com>
2024-04-20 03:15:22 +0000

    2024年4月20日 11:15

--- 04.TypeScript/P25.进阶类型兼容/index.ts
+++ 04.TypeScript/P25.进阶类型兼容/index.ts
@@ -5,12 +5,41 @@
 
 // 举例
 // 主类型
+// interface A {
+//     name:string
+//     age:number
+// }
+ 
+// // 子类型
+// interface B {
+//     name:string
+//     age:number
+//     sex:string
+// }
+ 
+// let a:A = {
+//     name:"老墨我想吃鱼了",
+//     age:33,
+// }
+ 
+// let b:B = {
+//     name:"老墨我不想吃鱼",
+//     age:33,
+//     sex:"女"
+// }
+ 
+// a = b
+// console.log('a: ', a);
+
+// A B 两个类型完全不同但是竟然可以赋值并无报错B类型充当A类型的子类型，当子类型里面的属性满足A类型就可以进行赋值，也就是说不能少可以多，这就是协变。
+
+// 逆变
+// 逆变一般发生于函数的参数上面
 interface A {
     name:string
     age:number
 }
  
-// 子类型
 interface B {
     name:string
     age:number
@@ -29,6 +58,15 @@ let b:B = {
 }
  
 a = b
-console.log('a: ', a);
-
-// A B 两个类型完全不同但是竟然可以赋值并无报错B类型充当A类型的子类型，当子类型里面的属性满足A类型就可以进行赋值，也就是说不能少可以多，这就是协变。
+ 
+let fna = (params:A) => {
+ 
+}
+let fnb = (params:B) => {
+    
+}
+ 
+// fna = fnb //错误
+ 
+// fnb = fna //正确
+// 这里比较绕，注意看fna 赋值 给 fnb 其实最后执行的还是fna 而 fnb的类型能够完全覆盖fna 所以这一定是安全的，相反fna的类型不能完全覆盖fnb少一个sex所以是不安全的。

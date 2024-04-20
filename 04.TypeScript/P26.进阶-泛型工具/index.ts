9f3d6bfd1d6d92c1c0f0a7bd27044ef850bcac85
Fly <723887809@qq.com>
2024-04-20 15:20:03 +0000

    2024年4月20日 23:20

--- 04.TypeScript/P26.进阶-泛型工具/index.ts
+++ 04.TypeScript/P26.进阶-泛型工具/index.ts
@@ -1,11 +1,25 @@
 /**
  * 泛型工具 TS 内置
- * Partial 所有属性 可选的意思是 接受一个泛型
+ * Partial 所有属性 可选的意思是 接受一个泛型，泛型里边的属性都会变成可选的属性
  * Required 所有属性必填的意思 
  * Pick 提取部分属性
  * Omit 忽略部分属性 并且返回新的类型
  * Exclude 排除部分属性
  */ 
 
+
+
+interface User {
+    name: string
+    age: number
+    address:string
+}
+
+type PartialUser = Partial<User>;// 所有属性 可选的意思是 接受一个泛型，泛型里边的属性都会变成可选的属性
+// type PartialUser = {
+//     name?: string | undefined;
+//     age?: number | undefined;
+//     address?: string | undefined;
+// }
 
 

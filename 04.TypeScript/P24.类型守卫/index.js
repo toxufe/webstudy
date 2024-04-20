78e0d0a677e9d8731dbd53680b5b781146edadb5
Fly <723887809@qq.com>
2024-04-20 02:39:34 +0000

    2024年4月20日 10:39

--- 04.TypeScript/P24.类型守卫/index.js
+++ 04.TypeScript/P24.类型守卫/index.js
@@ -1,6 +1,5 @@
 // 类型守卫 在代码中执行特定的检查以确定类型的方法 以及 确定变量的一个类型
 // 1. 类型收缩 | 类型收窄
-
 // typeof 是有缺陷的 比如说 数组 对象 函数 null 都会返回 object
 // 判断字符串
 // const isstring = (str:any)=> typeof str === 'string';
@@ -32,8 +31,6 @@
 // const isPromise = (promise: any)=> promise instanceof Promise;
 // // 判断Map
 // const isMap = (map: any)=> map instanceof Map;
-
-
 // // 例子 
 // console.log(isstring('zfpx')); // true
 // console.log(isArr([1,2,3])); // true
@@ -50,64 +47,45 @@
 // console.log(isError(new Error())); // true
 // console.log(isPromise(new Promise(()=>{}))); // true
 // console.log(isMap(new Map())); // true
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
 // 2. 类型谓词 | 自定义类型守卫
 // 实现一个函数 该函数可以传入任何类型
 // 但是如果是object 就检查里边的属性，如果属性是number的话就取两位小数
 // 如果是字符串的话就去除左右空格
 // 如果是函数的话就执行
-
 // const isObject = (val: any)=> Object.prototype.toString.call(val) === '[object Object]';
 // 简写
-const isObject = (val: any)=> ({}).toString.call(val) === '[object Object]';
+var isObject = function (val) { return ({}).toString.call(val) === '[object Object]'; };
 // 类型缩窄
-const isNum = (val: any) => typeof val === 'number';
-const isStr = (val: any) => typeof val === 'string';
-const isFn = (val: any) => typeof val === 'function';
-
+var isNum = function (val) { return typeof val === 'number'; };
+var isStr = function (val) { return typeof val === 'string'; };
+var isFn = function (val) { return typeof val === 'function'; };
 // 1. 没有提示 any 类型
 // 2. propertier of undefined (reading 'a')
 // node 环境 this undefined
-const fn = (data:any)=>{
-    if(isObject(data)){
+var fn = function (data) {
+    if (isObject(data)) {
         // console.log('data: ', data);  
         // // 遍历属性不能用for in 因为for in 会遍历原型链上的属性
         // for (const key in data) {
         //     console.log('forin-key: ', key,data[key]);
         // }
-
         // // Object.keys() 会返回一个由一个给定对象的自身可枚举属性组成的数组
         // console.log('Object.keys(): ', Object.keys(data));
-        let val;
-        Object.keys(data).forEach((key)=>{
-            val = data[key];
-            if(isNum(val)){
-                data[key] = val.toFixed(2);
+        var val_1;
+        Object.keys(data).forEach(function (key) {
+            val_1 = data[key];
+            if (isNum(val_1)) {
+                data[key] = val_1.toFixed(2);
             }
-            if(isStr(val)){
-                data[key] = val.trim();
+            if (isStr(val_1)) {
+                data[key] = val_1.trim();
             }
-            if(isFn(val)){
-                val();
+            if (isFn(val_1)) {
+                val_1();
             }
-        })
+        });
     }
-}
-
+};
 // let ccc = {
 //     aaa:"xxx",
 //     bbb:"xxx",
@@ -116,13 +94,12 @@ const fn = (data:any)=>{
 // let ddd = Object.create(ccc);
 // ddd.age = 18;
 // ddd.name = "zzz";
-
-let ddd= {
-    a:1000.22222222222222222,
-    b:"  zzz  ",
-    c(){
+var ddd = {
+    a: 1000.22222222222222222,
+    b: "  zzz  ",
+    c: function () {
         console.log(this);
         return this.a;
     },
-}
+};
 fn(ddd);

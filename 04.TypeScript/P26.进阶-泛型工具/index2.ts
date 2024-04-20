e9ec18170846bb28257d9a9be317e89ba6adc473
Fly <723887809@qq.com>
2024-04-20 15:52:50 +0000

    2024年4月20日 23:52

--- 04.TypeScript/P26.进阶-泛型工具/index2.ts
+++ 04.TypeScript/P26.进阶-泛型工具/index2.ts
@@ -8,9 +8,36 @@
 // * 1. Record
 // 约束对象的key和value
 // 所以接收两个泛型参数
-// key 必须全部满足 value 随便，不限制。
+// key 必须全部满足 value 随便 但是必须是一个类型
+
+type Key = "a"| "b" | "c";
+type Value = "1" | "2" | "3";
+
+// const obj:Record<Key,Value> = {
+//     a:"1",
+//     b:"2",
+//     c:"1"
+// };
 
 
+// 嵌套的写法
+const obj:Record<Key,Record<Key,Value>> = {
+    a:{
+        a:"1",
+        b:"2",
+        c:"3"
+    },
+    b:{
+        a:"1",
+        b:"2",
+        c:"3"
+    },
+    c:{
+        a:"1",
+        b:"1",
+        c:"3"
+    }
+};
 
 
 // * 2. ReturnType

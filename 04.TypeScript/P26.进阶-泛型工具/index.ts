30e1bc3231138de1f855b5781116a8fc65d7db8b
Fly <723887809@qq.com>
2024-04-20 15:17:21 +0000

    2024年4月20日 23:17

--- 04.TypeScript/P26.进阶-泛型工具/index.ts
+++ 04.TypeScript/P26.进阶-泛型工具/index.ts
@@ -1,8 +1,11 @@
 /**
  * 泛型工具 TS 内置
- * Partial 属性 可选的意思是
- * Required 属性必填的意思 
+ * Partial 所有属性 可选的意思是
+ * Required 所有属性必填的意思 
  * Pick 提取部分属性
  * Omit 忽略部分属性 并且返回新的类型
  * Exclude 排除部分属性
- */
+ */ 
+
+
+

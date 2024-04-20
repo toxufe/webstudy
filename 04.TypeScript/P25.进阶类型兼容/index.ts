40a9236426a60c92e4be173f78cbcfee707535e8
Fly <723887809@qq.com>
2024-04-20 03:04:21 +0000

    2024年4月20日 11:04

--- 04.TypeScript/P25.进阶类型兼容/index.ts
+++ 04.TypeScript/P25.进阶类型兼容/index.ts
@@ -7,27 +7,26 @@
 interface A {
     name:string
     age:number
-    sex:string
 }
  
 interface B {
     name:string
     age:number
-   
+    sex:string
 }
  
 let a:A = {
     name:"老墨我想吃鱼了",
     age:33,
-    sex:"女"
 }
  
 let b:B = {
     name:"老墨我不想吃鱼",
     age:33,
-   
+    sex:"女"
 }
  
 a = b
+console.log('a: ', a);
 
 // A B 两个类型完全不同但是竟然可以赋值并无报错B类型充当A类型的子类型，当子类型里面的属性满足A类型就可以进行赋值，也就是说不能少可以多，这就是协变。

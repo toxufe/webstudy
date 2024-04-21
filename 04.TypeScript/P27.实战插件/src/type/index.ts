6184b1a3aed39c0fb2dbbfe31c02d24e83e1cc9b
Fly <723887809@qq.com>
2024-04-21 16:07:39 +0000

    2024年4月22日 00:07

--- 04.TypeScript/P27.实战插件/src/type/index.ts
+++ 04.TypeScript/P27.实战插件/src/type/index.ts
@@ -1,9 +1,10 @@
 // expire 过期时间key permanent 永久不过期
 import { Dictionaries } from "../enum";
 export type Key = string;
+export type Expire = Dictionaries.permanent | number;// 过去时间支持时间戳
 export interface StorageCls {
     get:()=>void;
-    set:<T>(key:Key,value:T,expire:Dictionaries.permanent)=>void;
+    set:<T>(key:Key,value:T,expire:Expire)=>void;
     remove:()=>void;
     clear:()=>void;
 }

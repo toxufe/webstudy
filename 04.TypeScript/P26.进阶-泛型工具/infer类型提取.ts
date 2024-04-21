de314325c27fc26a70aa1a6e31c5858515b75f62
Fly <723887809@qq.com>
2024-04-21 15:39:02 +0000

    2024年4月21日 23:39

--- 04.TypeScript/P26.进阶-泛型工具/infer类型提取.ts
+++ 04.TypeScript/P26.进阶-泛型工具/infer类型提取.ts
@@ -1,1 +1,13 @@
-// 1. 提取头部元素
+// // 1. 提取数组的头部元素 或 尾部元素
+// type Arr = ['a','b','c'];
+// // 头部
+// // type A<T extends any[]> = T extends [infer one ,...any[]]? one : [];
+// // 尾部
+// type A<T extends any[]> = T extends [...any[],infer one ]? one : [];
+// type Ac = A<Arr>;
+// // type Ac = "a" // 头部 
+// // type Ac = "c" // 尾部
+
+
+
+// 删除数组的第一个元素类型

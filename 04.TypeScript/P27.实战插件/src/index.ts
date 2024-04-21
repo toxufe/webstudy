32d2f0c73b193937a8d4e32be014993019ed3b45
Fly <723887809@qq.com>
2024-04-21 16:08:13 +0000

    2024年4月22日 00:08

--- 04.TypeScript/P27.实战插件/src/index.ts
+++ 04.TypeScript/P27.实战插件/src/index.ts
@@ -1,8 +1,8 @@
 // expire 过期时间key permanent 永久不过期
-import { StorageCls,Key } from "./type";
+import { StorageCls,Key,Expire } from "./type";
 import { Dictionaries } from "./enum";
 export class Storage implements StorageCls {
-    set<T>(key:Key,value:T,expire:Dictionaries.permanent){
+    set<T>(key:Key,value:T,expire:Expire=Dictionaries.permanent){
         console.log('get');
     }
     get() {

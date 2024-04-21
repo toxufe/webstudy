7f8472366a4118163d0a6df104863b8431f8baf4
Fly <723887809@qq.com>
2024-04-21 15:40:46 +0000

    2024年4月21日 23:40

--- 04.TypeScript/P26.进阶-泛型工具/infer类型提取.ts
+++ 04.TypeScript/P26.进阶-泛型工具/infer类型提取.ts
@@ -10,4 +10,14 @@
 
 
 
-// 删除数组的第一个元素类型
+// 删除数组的第一个元素类型 
+type Arr = ['a','b','c'];
+type Shift<T extends any[]> = T extends [unknown,...infer U] ? U : [];
+type ShiftArr = Shift<Arr>;
+// type ShiftArr = ["b", "c"]
+
+// 删除数组的最后一个元素类型
+type Pop<T extends any[]> = T extends [...infer U,unknown] ? U : [];
+type PopArr = Pop<Arr>;
+// type PopArr = ["a", "b"]
+

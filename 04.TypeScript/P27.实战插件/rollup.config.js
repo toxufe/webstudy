5616fd4a6495bbaa1cb1d1a05f39761545833c7e
Fly <723887809@qq.com>
2024-04-21 16:46:44 +0000

    2024年4月22日 00:46

--- 04.TypeScript/P27.实战插件/rollup.config.js
+++ 04.TypeScript/P27.实战插件/rollup.config.js
@@ -2,8 +2,11 @@ import ts from 'rollup-plugin-typescript2'
 import path from 'path'
 import { fileURLToPath } from 'url'
 const metaUrl = fileURLToPath(import.meta.url)
-console.log('metaUrl: ', metaUrl);
+
 const dirName = path.dirname(metaUrl)
+console.log('import.meta.url: ', import.meta.url);
+console.log('metaUrl: ', metaUrl);
+console.log('dirName: ', dirName);
 export default {
     input: 'src/index.ts',
     output:{

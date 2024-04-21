a4ae3d7969786c6377e3819c8ef522f958f07025
Fly <723887809@qq.com>
2024-04-21 15:19:20 +0000

    2024年4月21日 23:19

--- 04.TypeScript/P26.进阶-泛型工具/inter学习.ts
+++ 04.TypeScript/P26.进阶-泛型工具/inter学习.ts
@@ -1,5 +1,6 @@
 // infer 就是推导泛型的参数
 // infer 声明只能出现在 extends 的子语句中
+// infer 后边跟变量名
 // 获取promise的返回值
 // infer 协变
 // infer 逆变
@@ -7,3 +8,15 @@
 
 
 // 获取promise的返回值
+interface User {
+    name: string;
+    age: number;
+}
+// type PromiseType = Promise<User>;
+// type GetPromiseType<T> = T extends Promise<infer U> ? U : T;
+
+// 多层Promise
+type PromiseType = Promise<Promise<Promise<User>>>;
+type GetPromiseType<T> = T extends Promise<infer U> ? U : T;
+type T = GetPromiseType<PromiseType>; // User
+// type T = User

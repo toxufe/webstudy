3b8e09bf6c965e78bd37aa2649b3a45ea8ecdcea
Fly <723887809@qq.com>
2024-04-22 16:04:27 +0000

    2024年4月23日 00:04

--- 06.vueRouter/lesson01/router/src/components/login.vue
+++ 06.vueRouter/lesson01/router/src/components/login.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import {} from "vue"
+import { useRouter } from "vue-router";
 import {data} from './list.json'
+
+const router = useRouter();
 
 type Item = {
   name: string;
   price: string;
   id: number;
+}
+/**
+ * query 传参是在url上显示的
+ */
+
+const toDetail = (item: Item) => {
+  // query只能传递对象
+  router.push({
+    path: '/reg',
+    query: item
+  })
 }
 
 
@@ -30,7 +44,7 @@ type Item = {
       <td>{{ item.id }}</td>
       <td>{{ item.name }}</td>
       <td>{{ item.price }}</td>
-      <td><button>详情</button></td>
+      <td><button @click="toDetail(item)">详情</button></td>
     </tr>
   </tbody>
 </table>
--- 06.vueRouter/lesson01/router/src/components/reg.vue
+++ 06.vueRouter/lesson01/router/src/components/reg.vue
@@ -5,10 +5,10 @@ import {} from "vue"
 <template>
   <h1>嘿嘿嘿 我是详情页面</h1>
   <hr>
-  <p>id: {{ $route.params.id }}</p>
+  <p>id: {{ $route.query.id }}</p>
   <p>name: {{ $route.query.name }}</p>
   <p>price: {{ $route.query.price }}</p>
-  
+
 </template>
 
 <style scoped>

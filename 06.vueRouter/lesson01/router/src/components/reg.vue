fc5fe0f6fd624aa0ad33728f732988c409803684
Fly <723887809@qq.com>
2024-04-22 16:14:25 +0000

    2024年4月23日 00:14

--- 06.vueRouter/lesson01/router/src/components/reg.vue
+++ 06.vueRouter/lesson01/router/src/components/reg.vue
@@ -3,6 +3,7 @@ import {} from "vue"
 import { useRoute,useRouter } from "vue-router";
 // 取值的时候使用useRoute
 const route =  useRoute();
+console.log('route: ', route);
 const router = useRouter();
 </script>
 
@@ -12,11 +13,11 @@ const router = useRouter();
     <h1>嘿嘿嘿 我是详情页面</h1>
   </div>
 
-  <h2>query 传参</h2>
+  <!-- <h2>query 传参</h2>
   <hr>
   <p>id: {{ route.query.id }}</p>
   <p>name: {{ route.query.name }}</p>
-  <p>price: {{ route.query.price }}</p>
+  <p>price: {{ route.query.price }}</p> -->
 
   <h2>params 传参</h2>
   <hr>

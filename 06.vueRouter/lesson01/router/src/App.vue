15f6dd3a4f91b22d3b26fa76f3fcaa8529b50213
Fly <723887809@qq.com>
2024-04-22 03:47:10 +0000

    2024年4月22日 11:47

--- 06.vueRouter/lesson01/router/src/App.vue
+++ 06.vueRouter/lesson01/router/src/App.vue
@@ -5,7 +5,15 @@ import {} from "vue"
 <template>
   <h1>路由学习</h1>
   <hr>
+  <div>
+    <router-link to="/">登录</router-link>
+    <router-link to="/reg">注册</router-link>
+  </div>
   <RouterView></RouterView>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+a {
+  margin-right: 10px;
+}
+</style>

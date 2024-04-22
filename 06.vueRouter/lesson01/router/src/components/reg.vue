00628579f02ab1bcfc6c14504e80b2688255ec9e
Fly <723887809@qq.com>
2024-04-22 16:06:25 +0000

    2024年4月23日 00:06

--- 06.vueRouter/lesson01/router/src/components/reg.vue
+++ 06.vueRouter/lesson01/router/src/components/reg.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import {} from "vue"
+import { useRoute } from "vue-router";
+// 取值的时候使用useRoute
+const route =  useRoute();
 </script>
 
 <template>
   <h1>嘿嘿嘿 我是详情页面</h1>
   <hr>
-  <p>id: {{ $route.query.id }}</p>
-  <p>name: {{ $route.query.name }}</p>
-  <p>price: {{ $route.query.price }}</p>
+  <p>id: {{ route.query.id }}</p>
+  <p>name: {{ route.query.name }}</p>
+  <p>price: {{ route.query.price }}</p>
 
 </template>
 

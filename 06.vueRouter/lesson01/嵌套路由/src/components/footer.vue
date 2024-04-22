2252f0eb928af6a0cc2257f030658d35862b18bf
Fly <723887809@qq.com>
2024-04-23 00:48:54 +0800

    嵌套路由

--- 06.vueRouter/lesson01/嵌套路由/src/components/footer.vue
+++ 06.vueRouter/lesson01/嵌套路由/src/components/footer.vue
@@ -3,9 +3,11 @@ import {} from "vue"
 </script>
 
 <template>
-    <h1>我是父路由</h1>
-    <hr>
     <router-view></router-view>
+    <hr>
+    <h1>我是父路由</h1>
+    <router-link style="margin-right: 10px;" to="/">登录</router-link>
+    <router-link to="/reg/1">注册</router-link>
 </template>
 
 <style scoped></style>

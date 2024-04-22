d1e46670196905cdb17da9feaaa38788bfd237eb
Fly <723887809@qq.com>
2024-04-22 16:11:51 +0000

    2024年4月23日 00:11

--- 06.vueRouter/lesson01/router/src/components/reg.vue
+++ 06.vueRouter/lesson01/router/src/components/reg.vue
@@ -11,10 +11,18 @@ const router = useRouter();
     <button @click="router.back()">返回</button>
     <h1>嘿嘿嘿 我是详情页面</h1>
   </div>
+
+  <h2>query 传参</h2>
   <hr>
   <p>id: {{ route.query.id }}</p>
   <p>name: {{ route.query.name }}</p>
   <p>price: {{ route.query.price }}</p>
+
+  <h2>params 传参</h2>
+  <hr>
+  <p>id: {{ route.params.id }}</p>
+  <p>name: {{ route.params.name }}</p>
+  <p>price: {{ route.params.price }}</p>
 
 </template>
 

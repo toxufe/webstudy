48ef049a8089452355f9aebca1ac58842d5e305c
Fly <723887809@qq.com>
2024-04-22 16:10:47 +0000

    2024年4月23日 00:10

--- 06.vueRouter/lesson01/router/src/components/login.vue
+++ 06.vueRouter/lesson01/router/src/components/login.vue
@@ -12,13 +12,20 @@ type Item = {
 }
 /**
  * query 传参是在url上显示的
+ * createWebHashHistory 模式下会在url上显示参数
  */
 
 const toDetail = (item: Item) => {
   // query只能传递对象
+  // router.push({
+  //   path: '/reg',
+  //   query: item
+  // })
+
+  // params可以传递字符串
   router.push({
-    path: '/reg',
-    query: item
+    name: 'Reg',
+    params: item
   })
 }
 

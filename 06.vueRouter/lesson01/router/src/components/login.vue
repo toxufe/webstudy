44e057a6cd9f3d999b7cd428ca3a73015cada5a3
Fly <723887809@qq.com>
2024-04-22 15:55:16 +0000

    2024年4月22日 23:55

--- 06.vueRouter/lesson01/router/src/components/login.vue
+++ 06.vueRouter/lesson01/router/src/components/login.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import {} from "vue"
 import {data} from './list.json'
-console.log('data: ', data);
 </script>
 
 <template>
 <h1>嘿嘿嘿我是列表页面</h1>
+
+<table>
+  <thead>
+    <tr>
+      <th>id</th>
+      <th>品牌</th>
+      <th>价格</th>
+      <th>操作</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr></tr>
+  </tbody>
+</table>
 </template>
 
 <style scoped>

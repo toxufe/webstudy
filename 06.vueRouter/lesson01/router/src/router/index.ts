fa2e91c419c507703efb72d74ac7bafdece36c69
Fly <723887809@qq.com>
2024-04-22 03:48:41 +0000

    2024年4月22日 11:48

--- 06.vueRouter/lesson01/router/src/router/index.ts
+++ 06.vueRouter/lesson01/router/src/router/index.ts
@@ -1,6 +1,13 @@
 import {createRouter,createWebHistory,RouteRecordRaw} from 'vue-router'
 
 
+
+//vue2 mode history vue3 createWebHistory
+//vue2 mode  hash  vue3  createWebHashHistory
+//vue2 mode abstact vue3  createMemoryHistory SSR 服务端渲染
+
+
+
 const routes:Array<RouteRecordRaw> = [{
     path:'/',
     component:()=>import('../components/login.vue'),

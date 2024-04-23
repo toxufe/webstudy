428b74a0ff016ba414459b3fb7f85d4a0becd53f
Fly <723887809@qq.com>
2024-04-23 15:42:38 +0000

    2024年4月23日 23:42

--- 06.vueRouter/lesson01/导航守卫/src/components/Login.vue
+++ 06.vueRouter/lesson01/导航守卫/src/components/Login.vue
@@ -1,35 +1,55 @@
 <script setup lang="ts">
 import { ref,reactive } from "vue"
 import { useRouter } from "vue-router";
+import type {FormItemRule,FormInstance} from 'element-plus';
+import { ElMessage } from 'element-plus'
 const router = useRouter();
-const rules = reactive({
-    user:[
-        {required:true,message:'请输入账号',trigger:'blur'}
-    ],
-    password:[
-        {required:true,message:'请输入密码',trigger:'blur'}
-    ]
-});
-const form = ref({
-  user:'',
+
+const form = ref<FormInstance>();
+type Form = {
+    user:string,
+    password:string
+}
+const formData = ref<Form>({
+  user:'', 
   password:''
 })
 
+type Rules = {
+    [K in keyof Form]:Array<FormItemRule>
+}
+const rules = reactive<Rules>({
+    user:[
+        {required:true,message:'请输入账号',type:'string'}
+    ],
+    password:[
+        {required:true,message:'请输入密码',type:'string'}
+    ]
+});
+
+
 const onSubmit = ()=>{
-    console.log(form.value)
-    router.push('/index')
+    form.value?.validate((result)=>{
+        if(result){
+            router.push('/index');
+            localStorage.setItem('token','1');
+        }else{
+            ElMessage.error("请输入完整……");
+        }
+
+    });
 }
 </script>
 
 <template>
   <div class="login">
-    <el-card>
-     <el-form  ref="form" :model="form" :rules="rules" label-width="80px">
+    <el-card class="box-card ">
+     <el-form ref="form" :model="formData" :rules="rules" label-width="80px">
        <el-form-item label="账号" prop="user">
-         <el-input placeholder="请输入账号"  v-model="form.user"></el-input>
+         <el-input placeholder="请输入账号"  v-model="formData.user"></el-input>
        </el-form-item>
        <el-form-item label="密码" prop="password">
-        <el-input placeholder="请输入密码" v-model="form.password"></el-input>
+        <el-input placeholder="请输入密码" v-model="formData.password"></el-input>
        </el-form-item>
        <el-form-item>
          <el-button @click="onSubmit" type="primary">登录</el-button>
--- 06.vueRouter/lesson01/导航守卫/src/main.ts
+++ 06.vueRouter/lesson01/导航守卫/src/main.ts
@@ -5,5 +5,21 @@ import router from './router'
 import ElementUi from 'element-plus'
 import 'element-plus/dist/index.css'
 
-createApp(App).use(router).use(ElementUi).mount('#app')
+
+const app = createApp(App);
+app.use(router);
+app.use(ElementUi);
+
+
+// 路由中间件 路由前置守卫 授权登录使用
+const whiteList = ['/'];
+router.beforeEach((to, from, next) => {
+    if(whiteList.includes(to.path) || localStorage.getItem('token')) {
+        next();
+    }else{
+        next({path: '/'});
+    }
+})
+
+app.mount('#app')
  

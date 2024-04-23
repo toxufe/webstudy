fefc8f1906efa73a5556062a4f90c0c0854f0d21
Fly <723887809@qq.com>
2024-04-23 15:45:40 +0000

    2024年4月23日 23:45

---
+++ 06.vueRouter/lesson01/导航守卫/src/components/loadingBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {} from "vue"
+</script>
+
+<template>
+  <div class="wraps">
+    <div class="bar"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>

623074326d639db9b54948664cae89f72998d1e6
Fly <723887809@qq.com>
2024-04-25 15:30:58 +0000

    2024年4月25日 23:30

--- 09.微前端/webcomponentsDemo/index.js
+++ 09.微前端/webcomponentsDemo/index.js
@@ -3,7 +3,9 @@ window.onload = () => {
     class Wujie extends HTMLElement {
         constructor() {
             super();
+            // 创建shadowdom
         }
     }
-    // 挂载
+    // 挂载 不支持驼峰命名
+    window.customElements.define('wu-jie', Wujie);
 };

af030a6cd14811244ed8890d2e5f1db925826f67
Fly <723887809@qq.com>
2024-04-26 15:26:57 +0000

    2024年4月26日 23:26

--- 09.微前端/monorepo架构/main/src/main.ts
+++ 09.微前端/monorepo架构/main/src/main.ts
@@ -3,11 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Wujie from 'wujie-vue3'
 
 // import {api} from 'common'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Wujie)
 
 app.mount('#app')

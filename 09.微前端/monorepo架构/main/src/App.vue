024e60d56bd7ce3cef300dd0fe77c2db35ae836f
Fly <723887809@qq.com>
2024-04-26 15:31:15 +0000

    2024年4月26日 23:31

--- 09.微前端/monorepo架构/main/src/App.vue
+++ 09.微前端/monorepo架构/main/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import {} from "vue"
+import { } from "vue"
 </script>
 
 <template>
- <h1>这是主应用</h1>
+  <h1>这是主应用</h1>
+  <WujieVue name="react" url="http://localhost:5174/"></WujieVue>
+  <WujieVue name="vue" url="http://localhost:5175/"></WujieVue>
 </template>
 
 <style lang="scss" scoped></style>

ccc1a435c032231f0f0f1b129deb7ba709bb8a20
Fly <723887809@qq.com>
2024-04-26 16:15:44 +0000

    2024年4月27日 00:15

--- 09.微前端/wujie-vue3原理/webpack.config.js
+++ 09.微前端/wujie-vue3原理/webpack.config.js
@@ -1,2 +1,10 @@
-const config = {};
+const { Configuration } = require('webpack');
+const path = require('path');
+// 通过注解的方式给对象提供声明
+/**
+ * @type {Configuration}
+ */
+const config = {
+    
+};
 module.exports = config;

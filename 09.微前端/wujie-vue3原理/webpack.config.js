f221f29fb17c9b74b52e9648e98d903a43ca33aa
Fly <723887809@qq.com>
2024-04-26 16:18:57 +0000

    2024年4月27日 00:18

--- 09.微前端/wujie-vue3原理/webpack.config.js
+++ 09.微前端/wujie-vue3原理/webpack.config.js
@@ -5,10 +5,19 @@ const path = require('path');
  * @type {Configuration}
  */
 const config = {
+    mode: 'none',// 模式
     entry:"./src/index.ts",
     output:{
         filename:"index.js",
         path:path.resolve(__dirname,'lib')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.ts$/,
+                use:'ts-loader'
+            }
+        ]
     }
 };
 module.exports = config;

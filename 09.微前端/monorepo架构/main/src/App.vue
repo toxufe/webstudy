c89d64a8503c666bb6265ac648a332eb9afda332
Fly <723887809@qq.com>
2024-04-26 17:04:09 +0000

    2024年4月27日 01:04

--- 09.微前端/monorepo架构/main/src/App.vue
+++ 09.微前端/monorepo架构/main/src/App.vue
@@ -5,9 +5,9 @@ import { } from "vue"
 <template>
   <h1>这是主应用</h1>
   <!-- 子应用 React -->
-  <WujieVue name="react" url="http://localhost:5174/"></WujieVue>
+  <WuJieVue name="react" url="http://localhost:5174/"></WuJieVue>
   <!-- 子应用 Vue -->
-  <WujieVue name="vue" url="http://localhost:5175/"></WujieVue>
+  <WuJieVue name="vue" url="http://localhost:5175/"></WuJieVue>
 </template>
 
 <style lang="scss" scoped></style>

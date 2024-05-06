2079a1593714f79fad5006b0c0304e1163e361b9
Fly <723887809@qq.com>
2024-05-07 00:19:00 +0800

    重学ajax

--- 12.前端网络课程/重学Ajax/index.ts
+++ 12.前端网络课程/重学Ajax/index.ts
@@ -1,14 +1,49 @@
 import express from 'express'
 import fs from 'fs'
+import multer from 'multer'
+import path from 'path';
+
 
 const app = express();
+
+
+// 设置存储引擎
+const storage = multer.diskStorage({
+destination: function (req:any, file:any, cb:any) {
+    cb(null, 'uploads/') // 上传的文件将保存在 uploads 目录
+},
+filename: function (req:any, file:any, cb:any) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+}
+});
+
+// 创建 Multer 实例
+const upload = multer({ storage: storage });
 
 // 添加全局跨域 
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','*');
+    res.setHeader('Access-Control-Allow-Methods','*');
+    res.setHeader('Access-Control-Allow-Credentials','true');
     next();
 })
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+// 后端post请求
+app.post('/api/post',(req,res)=>{
+    console.log(req.body.name,req.body.ccc);
+    res.json({message:'post请求成功'})
+});
+
+// 上传文件
+app.post('/api/upload',upload.single('file'),(req:any,res)=>{
+    console.log(req.files);
+    res.json({message:'上传成功'})
+});
 
 app.get('/api/jsonp',(req,res)=>{
     const {callback} = req.query;
@@ -27,6 +62,7 @@ app.get('/api/json',(req,res)=>{
 })
 
 app.get('/api/text',(req,res)=>{
+    console.log(req.query.name);
     const data = fs.readFileSync('./index.txt','utf-8');
     res.send(data);
 });

a6840c15520344412e2d53fa0b0be4fb199a03e9
Fly <723887809@qq.com>
2024-05-13 14:04:12 +0000

    2024年5月13日 22:04

--- 12.前端网络课程/JWT/index.ts
+++ 12.前端网络课程/JWT/index.ts
@@ -3,11 +3,25 @@ import jwt from 'jsonwebtoken';
 import cors from 'cors';
 
 let app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 
+let user = {
+    username: 'admin',
+    password:'123456'
+};
 // 1. 登录 返回前端token 用于授权
-// 2. 列表接口但是必须是授权状态才能访问,否则是403
+app.post('/api/login',(req,res)=>{
+    let {username,password} = req.body;
+    if(username === user.username && password === user.password){
+        let token = jwt.sign
+    }else{
+        res.status(403).json({message:'用户名或密码错误'});
+    }
+});
+// 2. 列表接口但是必须是授权状态才能访问,否则是403 
 
 
 app.listen(3000, () => {

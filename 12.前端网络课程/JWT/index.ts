69458a96f67cf85959a632eb82eaafbbe6a9ecad
Fly <723887809@qq.com>
2024-05-13 14:01:29 +0000

    2024年5月13日 22:01

--- 12.前端网络课程/JWT/index.ts
+++ 12.前端网络课程/JWT/index.ts
@@ -6,6 +6,9 @@ let app = express();
 app.use(cors());
 
 
+// 1. 登录 返回前端token 用于授权
+// 2. 列表接口但是必须是授权状态才能访问,否则是403
+
 
 app.listen(3000, () => {
     console.log('server is running at http://localhost:3000');

1bc4cc0e6f42030acd1cb274b6a41c29a64b2796
Fly <723887809@qq.com>
2024-06-27 15:12:21 +0000

    2024年6月27日 23:12

--- 13.nodejs/09.模块化/esm.js
+++ 13.nodejs/09.模块化/esm.js
@@ -2,6 +2,6 @@
 // package.json "type": "commonjs",
 
 
-
+// export default 导出 ems模块全局只能有一个
 import aaa from './esmtest.js';
 console.log('aaa: ', aaa);
--- 13.nodejs/09.模块化/esmtest.js
+++ 13.nodejs/09.模块化/esmtest.js
@@ -3,3 +3,6 @@
 export default {
     a:1
 }
+
+
+export const name = "小满";

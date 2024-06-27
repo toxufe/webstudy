32c4c93fe15b55cfab4b06abd1932a77777268b8
Fly <723887809@qq.com>
2024-06-27 15:07:25 +0000

    2024年6月27日 23:07

--- 13.nodejs/09.模块化/index.js
+++ 13.nodejs/09.模块化/index.js
@@ -1,2 +1,24 @@
 // commonjs 规范的五种模式
 // 1. 引入自己编写的模块
+// require('./test');
+
+// 2. 引入第三方模块 npm i md5
+// const md5 = require('md5');
+// console.log(md5('123456'));
+
+// 3. nodejs 内置模块 fs http net os child_process
+// const fs = require('node:fs');// 高版本可以添加node:
+// console.log('fs: ', fs);
+
+// 4. C++ 扩展 addon napi node-gyp .node
+// 5. 支持引入json文件
+// const data = require("./data.json");
+// console.log('data: ', data);
+
+// module.export 导出模块和对象和值本身
+// const data = require("./test.js");
+// console.log('data: ', data);
+
+// 可以使用解构的方式
+// const { success, error } = require("./test.js");
+// console.log('success: ', success);
---
+++ 13.nodejs/09.模块化/test.js
@@ -0,0 +1,7 @@
+console.log("引入自己编写的模块");
+
+// 导出模块
+module.exports = {
+    success:1,
+    error:1
+}

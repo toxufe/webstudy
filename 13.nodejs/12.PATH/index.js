f6e3dae2da9f17238976c3367ee3a0954ebd78ce
Fly <723887809@qq.com>
2024-07-15 07:54:57 +0000

    2024年7月15日 15:54

--- 13.nodejs/12.PATH/index.js
+++ 13.nodejs/12.PATH/index.js
@@ -14,3 +14,6 @@ console.log('basename1: ', basename1);
 // 2.dirname 返回给定路径的目录名
 const basename2 = path.dirname("C:\\Users\\Administrator\\Desktop\\1.html");
 console.log('basename2: ', basename2);
+
+// 3.extname 返回路径的扩展名 带点
+console.log(path.extname("/a/a/ccc.b.b.b.aaa"));

80e4be03a38ed54d1b9e1f5e2857fe43c5a79cb0
Fly <723887809@qq.com>
2024-07-15 07:52:36 +0000

    2024年7月15日 15:52

---
+++ 13.nodejs/12.PATH/index.js
@@ -0,0 +1,16 @@
+const path = require("node:path");
+// 1.basename 返回给定路径的最后一部分
+// window 操作系统兼容正斜杠的写法
+const basename = path.basename("C:\\Users\\Administrator\\Desktop\\1.html");
+console.log('basename: ', basename);
+const filename = path.basename("/fxx/xxx/1.html");
+console.log('filename: ', filename);
+
+// posix 处理不了window的路径
+const basename1 = path.posix.basename("C:\\Users\\Administrator\\Desktop\\1.html");
+console.log('basename1: ', basename1);
+
+
+// 2.dirname 返回给定路径的目录名
+const basename2 = path.dirname("C:\\Users\\Administrator\\Desktop\\1.html");
+console.log('basename2: ', basename2);

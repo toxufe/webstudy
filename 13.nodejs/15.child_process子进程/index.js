75500f26e120f7a8b677bf12a451e00a7b58a414
Fly <723887809@qq.com>
2024-07-25 15:46:14 +0000

    2024年7月25日 23:46

--- 13.nodejs/15.child_process子进程/index.js
+++ 13.nodejs/15.child_process子进程/index.js
@@ -1,19 +1,20 @@
 const {exec,execSync,spawn,spawnSync,execFile,execFileSync,fork} = require("child_process");
 
 // exec 异步方法 回调函数 返回 buffer 可以执行shell命令,或者和软件交互
-
-// execSync 同步方法
+// execSync 同步方法  执行较小的shell 立马拿到结果的shell 字节超过200kb报错
 // error 错误 stdout 标准输出 stderr 错误输出 
-exec("node -v",(error,stdout,stderr)=>{
-    if(error){
-        return error;
-    }
-    console.log("stdout.toString()",stdout.toString());
-    console.log("stdout.stderr()",stderr.toString());
-})
+// exec("node -v",(error,stdout,stderr)=>{
+//     if(error){
+//         return error;
+//     }
+//     console.log("stdout.toString()",stdout.toString());
+//     console.log("stdout.stderr()",stderr.toString());
+// })
 
-// 同步写法
-const nodeVersion = execSync("node -v").toString();
-console.log('nodeVersion: ', nodeVersion);
+// // 同步写法
+// const nodeVersion = execSync("node -v").toString();
+// console.log('nodeVersion: ', nodeVersion);
+
+execSync("mkdir test")
 
 

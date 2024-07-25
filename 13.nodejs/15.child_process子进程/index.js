a7fbe20d6e95bd286a3c1d5b9d8816869c37ba68
Fly <723887809@qq.com>
2024-07-25 15:43:16 +0000

    2024年7月25日 23:43

--- 13.nodejs/15.child_process子进程/index.js
+++ 13.nodejs/15.child_process子进程/index.js
@@ -3,7 +3,7 @@ const {exec,execSync,spawn,spawnSync,execFile,execFileSync,fork} = require("chil
 // exec 异步方法 回调函数 返回 buffer 可以执行shell命令,或者和软件交互
 
 // execSync 同步方法
-// error 错误 stdout 标准输出 stderr 错误输出
+// error 错误 stdout 标准输出 stderr 错误输出 
 exec("node -v",(error,stdout,stderr)=>{
     if(error){
         return error;
@@ -11,4 +11,9 @@ exec("node -v",(error,stdout,stderr)=>{
     console.log("stdout.toString()",stdout.toString());
     console.log("stdout.stderr()",stderr.toString());
 })
+
+// 同步写法
+const nodeVersion = execSync("node -v").toString();
+console.log('nodeVersion: ', nodeVersion);
+
 

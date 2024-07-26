74d35c97f8e2edc1c15814b646e670ae87786a39
Fly <723887809@qq.com>
2024-07-26 15:24:46 +0000

    2024年7月26日 23:24

--- 13.nodejs/15.child_process子进程/index.js
+++ 13.nodejs/15.child_process子进程/index.js
@@ -30,12 +30,12 @@ const {exec,execSync,spawn,spawnSync,execFile,execFileSync,fork} = require("chil
 // console.log('a: ', a.toString());
 
 // spawn 没有实时输出 
-const { stdout } = spawnSync("netstat",['-a'],{});// 第二歌参数是个数组 可以携带命令的参数 第三个参数是个对象 选项
-stdout.on("data", (data) => {
-    console.log(data.toString());
+const { stdout } = spawn("netstat",['-a'],{});// 第二歌参数是个数组 可以携带命令的参数 第三个参数是个对象 选项
+stdout.on("data", (msg) => {
+    console.log(msg.toString());
 })
 
-stdout.on("close", (data) => {
+stdout.on("close", () => {
     console.log("结束了……");
 })
 
